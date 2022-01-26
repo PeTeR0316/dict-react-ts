2b0d3c236a7e8bec2f1019b77ad8fbcc7e7f273f
PeTeR0316 <49382552+PeTeR0316@users.noreply.github.com>
2022-01-26 18:02:37 +0900

    220126_API_정보_분리

--- server/translate/translate.js
+++ server/translate/translate.js
@@ -1,17 +1,17 @@
 const request = require("request");
 const express = require("express");
 const router = express.Router();
+const  naver_api = require('../account');
 
 // api 요청 변수
 const api_url = "https://openapi.naver.com/v1/papago/n2mt";
-const client_id = "gLN7v2OL3WgxmWbijeZC";
-const client_secret = "ZS5CD1q3kX";
+const client_id = naver_api.NAVER_API.CLIENT_ID;
+const client_secret = naver_api.NAVER_API.CLIENT_SECRET;
 
 router.get('/keyword/:text', (req, res) => {
     const translateText = req.params.text; //번역할 문장
     let result = null;
 
-    console.log(translateText)
     // api 요청 옵션 (번역: 영어 -> 한글)
     const options = {
         url: api_url,
